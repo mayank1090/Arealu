f007ff9eda1c53d86ef4fa58da4bad192d8ce969
Mayank <pali28913@gmail.com>
2023-07-19 16:35:18 +0530

    first landing page

---
+++ script.js
@@ -0,0 +1,9 @@
+
+    const navbarToggle = document.querySelector('.navbar-toggle');
+    const navbarMenu = document.querySelector('.navbar-menu');
+  
+    navbarToggle.addEventListener('click', function () {
+      navbarMenu.classList.toggle('active');
+    });
+
+  
