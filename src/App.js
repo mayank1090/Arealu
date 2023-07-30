eaa7b252ca7f73d4a0a1c9498817cee949f48e20
Mayank <mayank.rezang@gmail.com>
2023-07-30 14:18:43 +0530

    next

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Landing from './components/landingPage/landing';
 import Process from './components/process/process';
+import Scan from './components/Scanpage/scan';
 
 function App() {
   return (
     <div className="App">
       {/* <Landing/> */}
-      <Process/>
+      {/* <Process/> */}
+      <Scan/>
     </div>
   );
 }
---
+++ src/components/Scanpage/scan.js
@@ -0,0 +1,71 @@
+import React from "react";
+import "./scan.css";
+
+export default function Scan() {
+  return (
+    <>
+      <div className="container wholeprnt ">
+        <div className="text-center">
+            <p className="uperhead">we made your skin & hair care</p>
+        </div>
+
+        <div className="text-center">
+            <p className="lowerhead">hyper - personalized</p>
+        </div>
+
+        <div className="d-flex scrollprnt ">
+            <div className="subdiv">
+                <div className="orngehead">
+                    <p className="ongrr">scan</p>
+                </div>
+
+                <div className="blackext">
+                    <p className="black">experience the power of our cutting- edge ai model as it scans your skin & hair with precision, providing valuable insight into its unique characteristics and needs. unlock a deeper understanding of your skin & hair condition, settings the stages for personalized skin & hair care solutions.</p>
+                </div>
+            </div>
+
+            <div className="subdiv">
+                <div className="orngehead">
+                    <p className="ongrr">analyze</p>
+                </div>
+
+                <div className="blackext">
+                    <p className="black">our advanced ai technology goes
+beyond superficial observations,
+meticulously analyzing every aspect
+of your skin & hair. discover a
+comprehensive analysis that delves
+into texture, hydration 
+levels, pigmentation, and more.
+empower yourself with actionable 
+information for targeted skincare & 
+haircare routines.</p>
+                </div>
+            </div>
+
+            <div className="subdiv">
+                <div className="orngehead">
+                    <p className="ongrr">enhance</p>
+                </div>
+
+                <div className="blackext">
+                    <p className="black">elevate your skincare & haircare 
+game with our ai powered solutions 
+that enhance your skin’s and hair 
+vitality and radiance. leverage 
+personalized recommendations 
+derived from the in-depth analysis, 
+including tailored product 
+suggestions, lifestyle adjustments, 
+and expert tips. unlock the potential
+for healthier, more vibrant skin and
+hair like never before </p>
+                </div>
+            </div>
+
+        </div>
+
+      </div>
+    </>
+  );
+}
